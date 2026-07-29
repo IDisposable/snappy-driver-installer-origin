4c06c17ac641b124febb4014dcedea2e941b7ef2
joodleberry <joodleberry@users.sourceforge.net>
2026-07-29 04:29:51 +0000

    v2.0.0

--- external/webp/mingw/msys/1.0/makewebp.bat
+++ external/webp/mingw/msys/1.0/makewebp.bat
@@ -1,25 +1,56 @@
+rem NOTES:
+rem need to update the following files when changing libwebp version
+rem "trunk\external\webp\mingw\msys\1.0\home\libwebp-*.tar.gz"
+rem "trunk\external\webp\mingw\msys\1.0\makewebp.bat"
+rem "trunk\external\webp\mingw\msys\1.0\home\makewebp.bat"
+
+rem %PREFIX% is the output path of the build
+rem add each of these paths to code blocks search path for each project build config
+
 echo Using %TOOLSET%
 
-if "%TOOLSET%"=="msvc" goto msvc
-
+set LIBWEBP_VER=1.6.0
 set HOME=%CD%\home
 
+cls
+@echo *** libwebp 32-bit build
+@echo.
+
 set MSYSTEM=MINGW32
-set PREFIX=%2
+set PREFIX=c:/mingw/mingw32/i686-w64-mingw32
+set PATH=C:\mingw\mingw32\bin;%PATH%
+@echo MSYSTEM: %MSYSTEM%
+@echo PREFIX : %PREFIX%
+@echo PATH   : %PATH%
+pause
+
+rem need a clean source path
+cd C:\msys64\home
+if exist libwebp-%LIBWEBP_VER%  rd libwebp-%LIBWEBP_VER% /s /q	
+if exist libwebp-%LIBWEBP_VER%.tar del libwebp-%LIBWEBP_VER%.tar /q
+"C:\Program Files\7-Zip\7z.exe" x libwebp-%LIBWEBP_VER%.tar.gz
+"C:\Program Files\7-Zip\7z.exe" x libwebp-%LIBWEBP_VER%.tar
+pause
 %1\sh --login -i %~dp0home\makewebp.bat
+
+cls
+@echo *** libwebp 64-bit build
+@echo.
 
 set MSYSTEM=MINGW64
-set PREFIX=%3
-%1\sh --login -i %~dp0home\makewebp.bat
-goto skip
+set PREFIX=c:/mingw/mingw64/x86_64-w64-mingw32
+set PATH=C:\mingw\mingw64\bin;%PATH%
+@echo MSYSTEM: %MSYSTEM%
+@echo PREFIX : %PREFIX%
+@echo PATH   : %PATH%
+pause
 
-:msvc
-%1\sh --login -i %~dp0home\unpack.bat
-cd home\libwebp-%LIBWEBP_VER%
-call "%MSVC_PATH%\VC\vcvarsall"
-nmake /f Makefile.vc CFG=release-static RTLIBCFG=static OBJDIR=output ARCH=x32
-nmake /f Makefile.vc CFG=debug-static RTLIBCFG=static OBJDIR=output ARCH=x32
-call "%MSVC_PATH%\VC\bin\amd64\vcvars64.bat"
-nmake /f Makefile.vc CFG=release-static RTLIBCFG=static OBJDIR=output ARCH=x64
-nmake /f Makefile.vc CFG=debug-static RTLIBCFG=static OBJDIR=output ARCH=x64
-:skip
+rem need a clean source path
+cd C:\msys64\home
+if exist libwebp-%LIBWEBP_VER% rd libwebp-%LIBWEBP_VER% /s /q
+if exist libwebp-%LIBWEBP_VER%.tar del libwebp-%LIBWEBP_VER%.tar /q
+"C:\Program Files\7-Zip\7z.exe" x libwebp-%LIBWEBP_VER%.tar.gz
+"C:\Program Files\7-Zip\7z.exe" x libwebp-%LIBWEBP_VER%.tar
+pause
+
+%1\sh --login -i %~dp0home\makewebp.bat
